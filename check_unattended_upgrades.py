9b61ea89bcaf8ff2dde6cb547aa25ade1b13f783
Josef Friedrich <josef@friedrich.rocks>
2022-11-19 18:41:22 +0100

    Add some boilerplate files to start a rewrite in Python

---
+++ check_unattended_upgrades.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+
+import argparse
+from argparse import ArgumentParser
+import nagiosplugin
+
+class OptionContainer:
+    pass
+
+opts: OptionContainer = OptionContainer()
+
+def get_argparser() -> ArgumentParser:
+    parser: ArgumentParser = argparse.ArgumentParser(
+        prog="check_zpool_scrub",  # To get the right command name in the README.
+    )
+    return parser
+
+
+@nagiosplugin.guarded(verbose=0)
+def main():
+    pass
+    global opts
+    opts = get_argparser().parse_args()
+
+if __name__ == "__main__":
+    main()
