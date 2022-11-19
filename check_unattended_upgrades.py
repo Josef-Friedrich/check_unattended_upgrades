9812437bae63070f22f9d73c8c23fbd33999ccc5
Josef Friedrich <josef@friedrich.rocks>
2022-11-19 22:24:09 +0100

    Configure argparse arguments

--- check_unattended_upgrades.py
+++ check_unattended_upgrades.py
@@ -2,26 +2,139 @@
 
 import argparse
 from argparse import ArgumentParser
-import nagiosplugin
+from nagiosplugin.runtime import guarded
+from typing import cast
 
 
 class OptionContainer:
     pass
 
+
 opts: OptionContainer = OptionContainer()
+
 
 def get_argparser() -> ArgumentParser:
     parser: ArgumentParser = argparse.ArgumentParser(
-        prog="check_zpool_scrub",  # To get the right command name in the README.
+        prog="check_unattended_upgrades",  # To get the right command name in the README.
     )
+
+    parser.add_argument(
+        "-A",
+        "--anacron",
+        help="Check if the package 'anacron' is installed.",
+    )
+
+    parser.add_argument(
+        "-a",
+        "--autoclean",
+        help="Check if the configuration 'APT::Periodic::AutocleanInterval' is set properly.",
+    )
+
+    parser.add_argument(
+        "-c",
+        "--critical",
+        help="Time interval since the last execution to result in a critical state (seconds).",
+    )
+
+    parser.add_argument(
+        "-D",
+        "--short-description",
+        help="Show a short description of this check plugin.",
+    )
+
+    parser.add_argument(
+        "-d",
+        "--download",
+        help="Check if the configuration 'APT::Periodic:Download-Upgradeable-Packages' is set properly.",
+    )
+
+    parser.add_argument(
+        "-e",
+        "--enable",
+        help="Check if the configuration 'APT::Periodic::Enable' is set properly",
+    )
+
+    parser.add_argument(
+        "-l",
+        "--lists",
+        help="Check if the configuration 'APT::Periodic::Update-Package-Lists' is set properly.",
+    )
+
+    parser.add_argument(
+        "-m",
+        "--mail",
+        help="Check if the configuration 'Unattended-Upgrade::Mail' is set properly.",
+    )
+
+    parser.add_argument(
+        "-n",
+        "--dry-run",
+        help="Check if 'unattended-upgrades --dry-run' is working. Warning: If you use this option the performance data last_ago is always 0 or near to 0.",
+    )
+
+    parser.add_argument(
+        "-p",
+        "--repo",
+        help="Check if 'Unattended-upgrades' is configured to include the specified custom repository.",
+    )
+
+    parser.add_argument(
+        "-R",
+        "--reboot",
+        help="Check if the machine needs a reboot.",
+    )
+
+    parser.add_argument(
+        "-r",
+        "--remove",
+        help="Check if the configuration 'Unattended-Upgrade::Remove-Unused- Dependencies' is set properly.",
+    )
+
+    parser.add_argument(
+        "-S",
+        "--security",
+        help="Check if 'Unattended-upgrades' is configured to handle security updates.",
+    )
+
+    parser.add_argument(
+        "-s",
+        "--sleep",
+        help="Check if the configuration 'APT::Periodic::RandomSleep' is set properly.",
+    )
+
+    parser.add_argument(
+        "-t",
+        "--systemd-timers",
+        help="Check if the appropriate Systemd timers are enabled ( apt-daily-upgrade.timer, apt-daily.timer ).",
+    )
+
+    parser.add_argument(
+        "-u",
+        "--unattended",
+        help="Check if the configuration 'APT::Periodic::Unattended-Upgrade' is set properly.",
+    )
+
+    parser.add_argument(
+        "-v",
+        "--version",
+        help="Show the version number.",
+    )
+
+    parser.add_argument(
+        "-w",
+        "--warning",
+        help="Time interval since the last execution to result in a warning state (seconds).",
+    )
+
     return parser
 
 
-@nagiosplugin.guarded(verbose=0)
+# @guarded(verbose=0)
 def main():
     pass
     global opts
-    opts = get_argparser().parse_args()
+    opts = cast(OptionContainer, get_argparser().parse_args())
+
 
 if __name__ == "__main__":
     main()
