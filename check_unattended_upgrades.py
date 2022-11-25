9869f900150273afce2d25ec0c1628f17c58d6d6
Josef Friedrich <josef@friedrich.rocks>
2022-11-25 21:50:55 +0100

    Extend argparse interface

--- check_unattended_upgrades.py
+++ check_unattended_upgrades.py
@@ -48,12 +48,14 @@ def get_argparser() -> ArgumentParser:
     parser.add_argument(
         "-A",
         "--anacron",
+        action="store_true",
         help="Check if the package 'anacron' is installed.",
     )
 
     parser.add_argument(
         "-a",
         "--autoclean",
+        metavar="CONFIG_VALUE",
         help="Check if the configuration 'APT::Periodic::AutocleanInterval' is set properly.",
     )
 
@@ -66,36 +68,42 @@ def get_argparser() -> ArgumentParser:
     parser.add_argument(
         "-D",
         "--short-description",
+        action="store_true",
         help="Show a short description of this check plugin.",
     )
 
     parser.add_argument(
         "-d",
         "--download",
+        metavar="CONFIG_VALUE",
         help="Check if the configuration 'APT::Periodic:Download-Upgradeable-Packages' is set properly.",
     )
 
     parser.add_argument(
         "-e",
         "--enable",
+        metavar="CONFIG_VALUE",
         help="Check if the configuration 'APT::Periodic::Enable' is set properly",
     )
 
     parser.add_argument(
         "-l",
         "--lists",
+        metavar="CONFIG_VALUE",
         help="Check if the configuration 'APT::Periodic::Update-Package-Lists' is set properly.",
     )
 
     parser.add_argument(
         "-m",
         "--mail",
+        metavar="CONFIG_VALUE",
         help="Check if the configuration 'Unattended-Upgrade::Mail' is set properly.",
     )
 
     parser.add_argument(
         "-n",
         "--dry-run",
+        action="store_true",
         help="Check if 'unattended-upgrades --dry-run' is working. Warning: If you use this option the performance data last_ago is always 0 or near to 0.",
     )
 
@@ -108,36 +116,42 @@ def get_argparser() -> ArgumentParser:
     parser.add_argument(
         "-R",
         "--reboot",
+        action="store_true",
         help="Check if the machine needs a reboot.",
     )
 
     parser.add_argument(
         "-r",
         "--remove",
+        metavar="CONFIG_VALUE",
         help="Check if the configuration 'Unattended-Upgrade::Remove-Unused- Dependencies' is set properly.",
     )
 
     parser.add_argument(
         "-S",
         "--security",
+        action="store_true",
         help="Check if 'Unattended-upgrades' is configured to handle security updates.",
     )
 
     parser.add_argument(
         "-s",
         "--sleep",
+        metavar="CONFIG_VALUE",
         help="Check if the configuration 'APT::Periodic::RandomSleep' is set properly.",
     )
 
     parser.add_argument(
         "-t",
         "--systemd-timers",
+        action="store_true",
         help="Check if the appropriate Systemd timers are enabled ( apt-daily-upgrade.timer, apt-daily.timer ).",
     )
 
     parser.add_argument(
         "-u",
         "--unattended",
+        metavar="CONFIG_VALUE",
         help="Check if the configuration 'APT::Periodic::Unattended-Upgrade' is set properly.",
     )
 
