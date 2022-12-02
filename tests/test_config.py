77f3daf12b52105628a1b559221f1203a4aef215
Josef Friedrich <josef@friedrich.rocks>
2022-12-02 17:00:41 +0100

    Fix some tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -33,7 +33,7 @@ class TestConfigDownload(unittest.TestCase):
 
 class TestConfigEnable(unittest.TestCase):
     def test_ok(self) -> None:
-        result = execute_main(["--enable", "7"])
+        result = execute_main(["--enable", "1"])
         result.assert_first_line("UNATTENDED_UPGRADES OK")
         result.assert_ok()
 
@@ -41,13 +41,13 @@ class TestConfigEnable(unittest.TestCase):
         result = execute_main(["--enable", "42"])
         result.assert_critical()
         result.assert_first_line(
-            "UNATTENDED_UPGRADES CRITICAL - Configuration value for “APT::Periodic::AutocleanInterval” unexpected! actual: 7 expected: 42"
+            "UNATTENDED_UPGRADES CRITICAL - Configuration value for “APT::Periodic::Enable” unexpected! actual: 1 expected: 42"
         )
 
 
 class TestConfigSleep(unittest.TestCase):
     def test_ok(self) -> None:
-        result = execute_main(["--sleep", "7"])
+        result = execute_main(["--sleep", "0"])
         result.assert_first_line("UNATTENDED_UPGRADES OK")
         result.assert_ok()
 
@@ -55,13 +55,13 @@ class TestConfigSleep(unittest.TestCase):
         result = execute_main(["--sleep", "42"])
         result.assert_critical()
         result.assert_first_line(
-            "UNATTENDED_UPGRADES CRITICAL - Configuration value for “APT::Periodic::AutocleanInterval” unexpected! actual: 7 expected: 42"
+            "UNATTENDED_UPGRADES CRITICAL - Configuration value for “APT::Periodic::RandomSleep” unexpected! actual: 0 expected: 42"
         )
 
 
 class TestConfigUnattended(unittest.TestCase):
     def test_ok(self) -> None:
-        result = execute_main(["--unattended", "7"])
+        result = execute_main(["--unattended", "1"])
         result.assert_first_line("UNATTENDED_UPGRADES OK")
         result.assert_ok()
 
@@ -69,13 +69,13 @@ class TestConfigUnattended(unittest.TestCase):
         result = execute_main(["--unattended", "42"])
         result.assert_critical()
         result.assert_first_line(
-            "UNATTENDED_UPGRADES CRITICAL - Configuration value for “APT::Periodic::AutocleanInterval” unexpected! actual: 7 expected: 42"
+            "UNATTENDED_UPGRADES CRITICAL - Configuration value for “APT::Periodic::Unattended-Upgrade” unexpected! actual: 1 expected: 42"
         )
 
 
 class TestConfigLists(unittest.TestCase):
     def test_ok(self) -> None:
-        result = execute_main(["--lists", "7"])
+        result = execute_main(["--lists", "1"])
         result.assert_first_line("UNATTENDED_UPGRADES OK")
         result.assert_ok()
 
@@ -83,35 +83,35 @@ class TestConfigLists(unittest.TestCase):
         result = execute_main(["--lists", "42"])
         result.assert_critical()
         result.assert_first_line(
-            "UNATTENDED_UPGRADES CRITICAL - Configuration value for “APT::Periodic::AutocleanInterval” unexpected! actual: 7 expected: 42"
+            "UNATTENDED_UPGRADES CRITICAL - Configuration value for “APT::Periodic::Update-Package-Lists” unexpected! actual: 1 expected: 42"
         )
 
 
 class TestConfigMail(unittest.TestCase):
     def test_ok(self) -> None:
-        result = execute_main(["--mail", "7"])
+        result = execute_main(["--mail", "logs@example.com"])
         result.assert_first_line("UNATTENDED_UPGRADES OK")
         result.assert_ok()
 
     def test_critical(self) -> None:
-        result = execute_main(["--mail", "42"])
+        result = execute_main(["--mail", "logs@xxx.xx"])
         result.assert_critical()
         result.assert_first_line(
-            "UNATTENDED_UPGRADES CRITICAL - Configuration value for “APT::Periodic::AutocleanInterval” unexpected! actual: 7 expected: 42"
+            "UNATTENDED_UPGRADES CRITICAL - Configuration value for “Unattended-Upgrade::Mail” unexpected! actual: logs@example.com expected: logs@xxx.xx"
         )
 
 
 class TestConfigRemove(unittest.TestCase):
     def test_ok(self) -> None:
-        result = execute_main(["--remove", "1"])
+        result = execute_main(["--remove", "true"])
         result.assert_first_line("UNATTENDED_UPGRADES OK")
         result.assert_ok()
 
     def test_critical(self) -> None:
-        result = execute_main(["--remove", "0"])
+        result = execute_main(["--remove", "false"])
         result.assert_critical()
         result.assert_first_line(
-            "UNATTENDED_UPGRADES CRITICAL - Configuration value for “APT::Periodic::Download-Upgradeable-Packages” unexpected! actual: 1 expected: 0"
+            "UNATTENDED_UPGRADES CRITICAL - Configuration value for “Unattended-Upgrade::Remove-Unused-Dependencies” unexpected! actual: true expected: false"
         )
 
 
